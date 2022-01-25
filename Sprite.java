bdf8c5809749f298e2c431385537d87dbbd66ee0
bentoncanderson <98131194+bentoncanderson@users.noreply.github.com>
2022-01-25 17:59:25 -0600

    Add files via upload

---
+++ Sprite.java
@@ -0,0 +1,64 @@
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import javax.imageio.ImageIO;
+public abstract class Sprite {
+	int x;
+	int y;
+	int w;
+	int h;
+	boolean touchingBrick = false;
+	
+	public abstract boolean update();
+	public abstract void draw(Graphics g, int num);
+	
+	public boolean isMario()
+	{
+		return false;
+	}
+	public boolean isBrick()
+	{
+		return false;
+	}
+	
+	public boolean isCoinBrick(int num)
+	{
+		return false;
+	}
+	public boolean isCoin()
+	{
+		return false;
+	}
+	 public boolean detectCollisions(Sprite s)
+	 {
+		 //moving right
+		 if ((x + w) < s.x)
+		 {
+			 return false;
+		 }
+		 //moving left
+		 else if (x > (s.x + s.w))
+		 {
+			 return false;
+		 }
+		 //moving up
+		 else if (y > (s.y + s.h))
+		 {
+			 return false;
+		 }
+		 //moving down
+		 else if ((y + h) < s.y)
+		 {
+			 return false;
+		 }
+		 touchingBrick = true;
+		 return true;
+	 }
+	 
+	 
+
+}
+
